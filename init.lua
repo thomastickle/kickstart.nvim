9bf65c394c0a73cc56a0d3a8b7d2cbed4d66f08f
Thomas Tickle <ttickle@gmail.com>
2026-07-14 09:50:24 -0400

    Add native language tooling

--- init.lua
+++ init.lua
@@ -695,18 +695,16 @@ do
   --  See `:help lsp-config` for information about keys and how to configure
   ---@type table<string, vim.lsp.Config>
   local servers = {
-    -- clangd = {},
-    -- gopls = {},
-    -- pyright = {},
-    -- rust_analyzer = {},
-    --
-    -- Some languages (like typescript) have entire language plugins that can be useful:
-    --    https://github.com/pmizio/typescript-tools.nvim
-    --
-    -- But for many setups, the LSP (`ts_ls`) will work just fine
-    -- ts_ls = {},
-
-    stylua = {}, -- Used to format Lua code
+    angularls = {},
+    clangd = {},
+    emmet_language_server = {},
+    jsonls = {},
+    kotlin_lsp = { cmd = { 'intellij-server' } },
+    perlnavigator = {},
+    pyright = {},
+    rust_analyzer = {},
+    vtsls = {},
+    yamlls = {},
 
     -- Special Lua Config, as recommended by neovim help docs
     lua_ls = {
@@ -762,7 +760,12 @@ do
   -- You can press `g?` for help in this menu.
   local ensure_installed = vim.tbl_keys(servers or {})
   vim.list_extend(ensure_installed, {
-    -- You can add other tools here that you want Mason to install
+    'eslint_d',
+    'jdtls',
+    'ktfmt',
+    'prettierd',
+    'stylua',
+    'tree-sitter-cli',
   })
 
   require('mason-tool-installer').setup { ensure_installed = ensure_installed }
@@ -785,11 +788,22 @@ do
     format_on_save = function(bufnr)
       -- You can specify filetypes to autoformat on save here:
       local enabled_filetypes = {
-        -- lua = true,
-        -- python = true,
+        c = true,
+        cpp = true,
+        css = true,
+        html = true,
+        javascript = true,
+        javascriptreact = true,
+        kotlin = true,
+        lua = true,
+        perl = true,
+        python = true,
+        rust = true,
+        typescript = true,
+        typescriptreact = true,
       }
       if enabled_filetypes[vim.bo[bufnr].filetype] then
-        return { timeout_ms = 500 }
+        return { timeout_ms = 1000, lsp_format = 'fallback' }
       else
         return nil
       end
@@ -799,12 +813,19 @@ do
     },
     -- You can also specify external formatters in here.
     formatters_by_ft = {
-      -- rust = { 'rustfmt' },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
-      -- You can use 'stop_after_first' to run the first available formatter from the list
-      -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      c = { 'clang_format' },
+      cpp = { 'clang_format' },
+      css = { 'prettierd', 'prettier', stop_after_first = true },
+      html = { 'prettierd', 'prettier', stop_after_first = true },
+      javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+      kotlin = { 'ktfmt' },
+      lua = { 'stylua' },
+      perl = { 'perltidy' },
+      python = { 'ruff_format' },
+      rust = { 'rustfmt' },
+      typescript = { 'prettierd', 'prettier', stop_after_first = true },
+      typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
     },
   }
 
@@ -827,8 +848,8 @@ do
   --    See the README about individual language/framework/plugin snippets:
   --    https://github.com/rafamadriz/friendly-snippets
   --
-  -- vim.pack.add { gh 'rafamadriz/friendly-snippets' }
-  -- require('luasnip.loaders.from_vscode').lazy_load()
+  vim.pack.add { gh 'rafamadriz/friendly-snippets' }
+  require('luasnip.loaders.from_vscode').lazy_load()
 
   -- [[ Autocomplete Engine ]]
   vim.pack.add { { src = gh 'saghen/blink.cmp', version = vim.version.range '1.*' } }
@@ -907,7 +928,31 @@ do
   vim.pack.add { { src = gh 'nvim-treesitter/nvim-treesitter', version = 'main' } }
 
   -- Ensure basic parsers are installed
-  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+  local parsers = {
+    'bash',
+    'c',
+    'cpp',
+    'css',
+    'diff',
+    'html',
+    'java',
+    'javascript',
+    'json',
+    'kotlin',
+    'lua',
+    'luadoc',
+    'markdown',
+    'markdown_inline',
+    'perl',
+    'python',
+    'query',
+    'rust',
+    'tsx',
+    'typescript',
+    'vim',
+    'vimdoc',
+    'yaml',
+  }
   require('nvim-treesitter').install(parsers)
 
   ---@param buf integer
